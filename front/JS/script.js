ca69ae6a1bde6c5da55e54029f8b7412b2958028
willi <exemple@test>
2023-05-31 09:43:35 +0200

    Avancé P5

--- front/JS/script.js
+++ front/JS/script.js
@@ -1,32 +1,40 @@
+const url = "http://localhost:3000/api/products/";
+const elementsContainer = document.getElementById("items");
+console.log('Coucou');
 
-const url = "http://localhost:3000/api/products/"
-const elementsContainer = document.getElementById("items")
-console.log('Coucou')
 const getArticles = () => {
-  console.log('Coucou')
-    fetch(url)
+  console.log('Coucou');
+  fetch(url)
     .then(function (res) {
-
-        return res.json()
-
+      return res.json();
     })
+    .then(function (data) {
+      console.log(elementsContainer);
+      console.log(data);
+      for (const product of data) {
+        console.log(product);
+        const article = document.createElement("article");
+        const link = document.createElement("a");
+        const img = document.createElement("img");
+        const h3 = document.createElement("h3");
+        const p = document.createElement("p");
 
-    .then(function (data){
-      console.log(elementsContainer)
-      console.log(data)
-        for(product of data) {
-          console.log(product)
-          elementsContainer.innerHTML += `<a href="./product.html?id=42">
-            <article>
-              <img src="${product.imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
-              <h3 class="productName">Kanap name1</h3>
-              <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-            </article>
-          </a>`
+        img.src = product.imageUrl;
+        img.alt = product.name;
+        h3.textContent = product.name;
+        p.textContent = product.description;
 
-        }
-
+        link.href = `./product.html?id=${product.id}`;
+        link.appendChild(article);
+        article.appendChild(img);
+        article.appendChild(h3);
+        article.appendChild(p);
+        elementsContainer.appendChild(link);
+      }
     })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
 
-}
-getArticles()
+getArticles();
